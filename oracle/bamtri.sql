51abda6027b73ad9025f257138014f2f1759f2c1
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-20 09:58:26 +0800

    [add] readme and base files

---
+++ oracle/bamtri.sql
@@ -0,0 +1,11 @@
+-- cheatsheet
+-- C-c <tab>: start SQLi session
+-- C-c C-l a: list all objects
+-- C-c C-l t: list table detail
+
+-- display all tables
+SELECT table_name FROM USER_TABLES;
+
+SELECT COUNT(*) FROM T_LOG;
+
+SELECT COUNT(*) FROM T_EMPLOYEE_BASE;
