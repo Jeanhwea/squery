0e47271c049c6d5860d6f05771bcf3ce6db2d429
Jinghui Hu <hujinghui@buaa.edu.cn>
2018-11-20 10:08:30 +0800

    [add] mysql

---
+++ mysql/user.sql
@@ -0,0 +1,19 @@
+USE mysql;
+SELECT user FROM user;
+
+-- create account
+CREATE USER ben IDENTIFIED BY 'password';
+-- rename account
+RENAME USER ben TO bforta;
+-- delete account
+DROP USER ben;
+-- change password
+SET PASSWORD FOR bforta = Password('new password');
+
+-- show privileges
+SHOW GRANTS FOR username;
+
+-- grant all privileges
+GRANT ALL PRIVILEGES ON database_name.*
+  TO 'username'@'localhost'
+  IDENTIFIED  BY 'password';
